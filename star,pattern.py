96e2f95053015dded4c23931c1c49bd970af60fb
suchitvanapilli <suchitvanapilli25@gmail.com>
2025-09-08 13:30:50 +0530

    python

--- star,pattern.py
+++
@@ -1,5 +0,0 @@
-#upper case
-
-rows = 10
-for i in range(1, rows + 1):
-    print(' ' * (rows - i) + '*' *(2 * i - 1))
--- star,rev,pattern.py
+++
@@ -1,5 +0,0 @@
-#lower case
-
-rows = 10
-for i in range (rows, 0, -1):
-    print(' '*(rows - i) +'*'*(2 *i -1))
