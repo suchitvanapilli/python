12dee464b31ea6bd953314c6a72608b202d55f16
suchitvanapilli <suchitvanapilli25@gmail.com>
2025-09-08 19:35:37 +0530

    on order

---
+++ string_slicing.py
@@ -0,0 +1,41 @@
+# example 1
+s = "Hello World"
+print(s[1])
+
+# example 2
+s = "Hello World"
+print(s[-1])
+                                    # ---> indexing
+                                    # ---> [start : stop]
+                                    # ---> [start: stop: step]
+# example 3
+s = "Hello World"
+print(s[1:3])
+
+# example 4
+s = "Hello World"
+print(s[1:-1])
+
+# example 5
+s = "Hello World"
+print(s[:3])
+
+# example 6
+s = "Hello World"
+print(s[2:])
+
+# example 7
+s = "Hello World"
+print(s[:-1])
+
+# example 8
+s = "Hello World"
+print(s[::2])            # ---> step is starting here 
+
+# example 9
+s = "Hello World"
+print(s[1::2])
+
+# example 10
+s = "Hello World"
+print(s[::-1])
